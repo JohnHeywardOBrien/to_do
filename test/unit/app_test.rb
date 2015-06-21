4856cde8bb57f3ee395adaadb6a54e47bf2763e0
John O'Brien <reachjohnnow@gmail.com>
2015-06-21 03:26:17 +0000

    Added more working tests on Todo

--- test/unit/app_test.rb
+++ test/unit/app_test.rb
@@ -1,14 +1,42 @@
 # test.rb
 require './test/test_helper'
 
-class MyTest < MiniTest::Test
+db_options = {adapter: 'sqlite3', database: 'todo_test'}
+ActiveRecord::Base.establish_connection(db_options)
+
+
+class CreateToDo < ActiveRecord::Migration
+  def change
+    create_table :todos do |t|
+      t.string :todo_description
+    end
+  end
+end
+
+begin
+  CreateToDo.new.change
+rescue ActiveRecord::StatementInvalid
+  # might work
+end
+
+
+class Todo < ActiveRecord::Base
+end
+
+
+class TodoTest < MiniTest::Test
 
   include Rack::Test::Methods
 
   def app
     ToDo
   end
-
+  
+  def test_exists
+    todo = Todo.new(:todo_description => 'a new todo')
+    assert_equal 'a new todo', todo.todo_description
+  end
+  
   def test_hello_world
     get '/'
     assert last_response.ok?
