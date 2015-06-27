18d85984cd899de36600e56c4f249f8ac89c92a3
John O'Brien <reachjohnnow@gmail.com>
2015-06-27 01:25:29 +0000

    More testing of tests

--- test/unit/sinatra_test.rb
+++ test/unit/sinatra_test.rb
@@ -14,16 +14,17 @@ class SinatraTest < MiniTest::Test
     @todos = Todo.all
   end
   
-  def test_index_page
+  def test_it_gets_index
     get '/'
     assert last_response.ok?
     assert_equal "Testing all the things!", last_response.body
     assert true
   end
   
-  def test_new_page
+  def test_it_gets_new_page_returns_todos
     get '/todos'
     assert last_response.ok?
+    assert last_response.body.include?(@todos.to_s)
     assert_empty []
     assert true 
   end
